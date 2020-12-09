26a2adacc1965eb7413056cdde3c7c7ff845d26b
Peter Tumulty <ptumulty923@gmail.com>
2020-12-09 08:25:45 -0500

    bug: fixed postcss config not a func issue

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? require('cssnano') : []
+    ...process.env.NODE_ENV === 'production' ? [require('cssnano')] : []
   ]
 };
